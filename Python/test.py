26dcfa9344ac690bc0cf753271b48472874dacdc
root <root@deluxxe-laptop.deluxxe.ch>
2014-08-10 16:39:48 +0200

    weniger Tags werden ausgelesen.

--- Python/test.py
+++ Python/test.py
@@ -17,10 +17,11 @@ def indexing(pathtoimage):
 	foldername = os.path.dirname(imagepath)
 	filename = os.path.basename(imagepath)
 	f = open(imagepath, 'rb')
-	tags = exifread.process_file(f)
+	tags = exifread.process_file(f, details=False)
+	f.close()
 	exif_datetimeoriginal = str(tags['EXIF DateTimeOriginal'])
 	datetime = exif_datetimeoriginal[:4] + "-" + exif_datetimeoriginal[5:7] + "-" + exif_datetimeoriginal[8:]
-	imagemodel = str(tags['Image ImageDescription'])
+	imagemodel = str(tags['Image Model'])
 	#TODO Kontrollen fehlen, Tags sind nicht einheitlich...
 	query = "insert into test (name, path, datetime, imagemodel) values ('%s', '%s', TIMESTAMP '%s', '%s');" %(filename, foldername, datetime, imagemodel)
 	status = database.write(query)
