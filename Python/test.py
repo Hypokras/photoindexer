b3fb3b4823588bb116ba9e76adf69fcce37769cc
root <root@deluxxe-laptop.deluxxe.ch>
2014-08-10 17:39:09 +0200

    Funktion zu loopen ueber alle Files in einem Ordner erstellt.

--- Python/test.py
+++ Python/test.py
@@ -17,14 +17,33 @@ def indexing(pathtoimage):
 	foldername = os.path.dirname(imagepath)
 	filename = os.path.basename(imagepath)
 	f = open(imagepath, 'rb')
-	tags = exifread.process_file(f, details=False)
-	f.close()
-	exif_datetimeoriginal = str(tags['EXIF DateTimeOriginal'])
-	datetime = exif_datetimeoriginal[:4] + "-" + exif_datetimeoriginal[5:7] + "-" + exif_datetimeoriginal[8:]
-	imagemodel = str(tags['Image Model'])
-	#TODO Kontrollen fehlen, Tags sind nicht einheitlich...
-	query = "insert into test (name, path, datetime, imagemodel) values ('%s', '%s', TIMESTAMP '%s', '%s');" %(filename, foldername, datetime, imagemodel)
-	status = database.write(query)
+	try:
+		tags = exifread.process_file(f, details=False)
+		f.close()
+		exif_datetimeoriginal = str(tags['EXIF DateTimeOriginal'])
+		datetime = exif_datetimeoriginal[:4] + "-" + exif_datetimeoriginal[5:7] + "-" + exif_datetimeoriginal[8:]
+		imagemodel = str(tags['Image Model'])
+		#TODO Kontrollen fehlen, Tags sind nicht einheitlich...
+		query = "insert into test (name, path, datetime, imagemodel) values ('%s', '%s', TIMESTAMP '%s', '%s');" %(filename, foldername, datetime, imagemodel)
+		status = database.write(query)
+	except:
+		logfile = open("/tmp/photoindexer.log", "a")
+		logfile.writelines(pathtoimage + "\n")
+		logfile.close()
+		return
 	return [foldername, filename, datetime, imagemodel, status]
+
+
+# Ueber alle Files im photofolder loopen
+
+def loopinfolder(photofolder):
+	#logfile erstellen.
+	logfile = open("/tmp/photoindexer.log", "a")
+	logfile.write("Dies ist das Logfile\n")
+	logfile.close()
+	for root, dirs, files in os.walk(photofolder):
+		for n in files:
+			indexing(root + "/" + n)
+
 
 
