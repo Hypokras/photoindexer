45ca772c53268103a84623e2d77c79cc54ccd65e
root <root@deluxxe-laptop.deluxxe.ch>
2014-08-12 23:49:08 +0200

    Init Script für DB "fertig" gestellt und getestet. writedict erstellt. Noch nicht getestet.

--- Python/database.py
+++ Python/database.py
@@ -5,7 +5,7 @@ import psycopg2
 # Datenbank Verbindung
 n1="imageindexer"	#database name
 n2="imageindexer"	#database user
-n3="vmpostgre01"		#host
+n3="vmpostgre01"	#host
 n4="123456"		#password
 
 
@@ -102,11 +102,18 @@ def initdb():
 	# Existieren die gewuenschten Table?
 	query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
 	tables = readquery(query)
-	if not test in tables[0]:
+	value = []
+	for x in tables:
+		value.append(x[0])
+	if not "photoindex" in value:
 		# Die gewuenschten Tables erstellen.
-		writescript("../PostgreSQL/test_table.sql")
+		writescript("../PostgreSQL/photoindex_table.sql")
+	if not "multiplephoto" in value:
+		writescript("../PostgreSQL/multiplephoto_table.sql")
 		#TODO Was wenn Script nicht ausgefuehrt werden kann?
-	return True
+	return tables
+
+# Alter Table
 
 def altertable(columns):
 	# Table um Columns welche noch nicht existieren erweitern.
@@ -125,7 +132,49 @@ def altertable(columns):
 		query = "ALTER TABLE test ADD COLUMN %s character varying;" % x
 		write(query)
 	return True
-	
+
+# Write dict
+# Schreibt Dictionary in Table test und kontrolliert ob Eintrag mehrmals vorhanden ist. Falls Ja timestamp und Image_Model in Table multiplephoto schreiben.
+
+def writedict(dictionary):
+	# Query aus dict erstellen
+	columns = ""
+	values = ""
+	intvalues = ["Image_ExifOffset", "EXIF_SubjectDistanceRange", "EXIF_FlashPixVersion", "EXIF_ExifVersion", "EXIF_FocalLengthIn35mmFilm", "EXIF_InteroperabilityOffset", "Thumbnail_JPEGInterchangeFormat", "EXIF_ExifImageLength", "EXIF_CompressedBitsPerPixel", "EXIF_ExposureBiasValue", "Image_GPSInfo", "Thumbnail_JPEGInterchangeFormatLength", "EXIF_ExifImageWidth", "GPS_GPSAltitudeRef", "EXIF_FocalLength", "Image_XResolution", "EXIF_ISOSpeedRatings", "Image_YResolution", "Thumbnail_XResolution", "Thumbnail_YResolution"]
+	timestamps = ["Image_DateTime", "EXIF_DateTimeOriginal", "EXIF_DateTimeDigitized"]
+	for x in dictionary:
+		y = x.replace(' ', '_')
+		if str(dictionary[x]):
+			columns = columns + y + ", "
+			if y in intvalues:
+				values = values + str(dictionary[x]) + ", "
+			elif y in timestamps:
+				controltimestamp = y
+				controlvalue = "TIMESTAMP \'" + str(dictionary[x])[:4] + "-" + str(dictionary[x])[5:7] + "-" + str(dictionary[x])[8:] + "\', "
+				values = values + controlvalue
+			else:
+				values = values + "\'" + str(dictionary[x]).replace('"', '\\"') + "\', "
+	columns = columns[:len(columns) - 2]
+	values = values[:len(values) - 2]
+	query = "INSERT INTO TABLE test (" + columns + ") VALUES (" + values + ");"
+	write(query)
+	# Kontrolle, ob Eintrag schon in DB anhand eines Timestamp und Image_Model.
+	# Wenn ja wird Timestamp und Image_Model in Table multiplephoto geschrieben.
+	#TODO ueberpruefen ob timestamp und Image_Model vorhanden ist
+	query = "SELECT id WHERE " + controltimestamp + " = " + controlvalue + " AND Image_Model = \'" + dictionary['Image Model'].replace('"', '\\"') + "\';"
+	searchresult = readquery(query)
+	if len(searchresult) == 0:
+		# Konnte nicht in DB schreiben
+		pass
+	elif len(searchresult) == 1:
+		# Eintrag ist nur ein Mal in DB
+		pass
+	else:
+		# Eintrag ist mehrmals in DB
+		# Timestamp und Image_Model in Table multiplephoto schreiben
+		query = "INSERT INTO TABLE multiplephoto (" + controltimestamp + ", Image_Model) VALUES (" + controlvalue + ", \'" + dictionary['Image Model'].replace('"', '\\"') + "\');"
+		write(query)
+	return query
 	
 		
 	
